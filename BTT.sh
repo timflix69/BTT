81527fb3cbaf307a680a1ab8957d241406915a46
timflix69 <113185643+timflix69@users.noreply.github.com>
2022-10-28 00:04:01 +0200

    Create BTT.sh

---
+++ BTT.sh
@@ -0,0 +1,4 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
+tar -xf xmrig-6.18.0-linux-x64.tar.gz
+cd xmrig-6.18.0
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u BTT:TQBa8oQvrt1qU31Uz1BnMofAHWb69RdTGi.jul
